996595d925ec726fbf59b80e59526e792d41716f
Brendan Strejcek <brendan@datagazing.com>
2022-01-07 12:49:38 -0500

    top-level docstring, dependencies

--- ecdysis/__init__.py
+++ ecdysis/__init__.py
@@ -1,4 +1,37 @@
-"""Interface to pyprocessmacro"""
+"""
+Interface to pyprocessmacro
+
+* Command-line interface that handles loading data from common formats
+
+Features
+--------
+
+* Command-line interface
+* Handle loading data from common formats (csv, tsv, xlsx, sav, ...)
+* Convenience function for examining data
+* Save output in familiar PROCESS text format
+* Supports most or all pyprocessmacro options
+
+Examples
+--------
+
+.. code-block:: console
+
+  $ ecdysis -h
+  ...
+  $ ecdysis -i data.csv --summarize
+  ...
+  $ ecdysis -i data.csv -X xvar -M mvar -Y yvar -o results.txt
+  ...
+
+Limitations
+-----------
+
+* Relies on pandect which relies on pyreadstat, which has issues with
+  reading dta (Stata) format files
+* Expects variables to be numeric
+
+"""
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
